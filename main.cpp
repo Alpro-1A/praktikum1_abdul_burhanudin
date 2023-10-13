02ad927d7a8806381f715de70ede46c7765f34c6
abdlburhan <adulsburhan@gmail.com>
2023-10-13 09:21:54 +0700

    menambahkan file main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream> // input output stream
+
+using namespace std;
+
+int main()
+{
+    cout <<"-----------------------------------------------\n";
+    cout << "Nama : Abdul Burhanudin" << endl << endl;
+    cout << "NPM : 2310631170001" << endl << endl;
+    cout << "Asal : Karawang" << endl << endl;
+    cout << "Kelas : 1A_Informatika" << endl << endl;
+    cout << "Cita-cita : Programmer" << endl << endl;
+    cout << "Motivasi Masuk Informatika : Ingin memperdalam bahasa pemrograman" << endl;
+    cout <<"-----------------------------------------------\n\n";
+    return 0;
+}
